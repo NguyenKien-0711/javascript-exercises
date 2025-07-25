e28e4a470cb61b77b983444603ca22c23d35901c
Nguyen Duc Kien <kiennd711@gmail.com>
2025-07-25 10:19:10 +0700

    Complete 08_removeFromArray

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...manyArgs) {
+    let res = arr.filter(item => {
+        
+        return (!manyArgs.includes(item));
+    })
+    return res;
 };
 
 // Do not edit below this line
