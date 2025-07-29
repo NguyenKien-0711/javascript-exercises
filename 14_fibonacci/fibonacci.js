84b1154b24367dbc2c66bd2331341be0a5f3a3bf
Nguyen Duc Kien <kiennd711@gmail.com>
2025-07-29 15:08:34 +0700

    Complete 14_fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    if (number == 0) return 0;
+    if (number < 0) return "OOPS"
+    let arr = [1, 1];
+    for (let i = 0; i < Number(number); ++i) {
+        arr.push(Number(arr[i]) + Number(arr[i + 1])); 
+    }
+    return Number(arr[number - 1]);
 };
 
 // Do not edit below this line
