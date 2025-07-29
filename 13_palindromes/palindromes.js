61b49d9371351ee8c4a31a958b219cf8c346b054
Nguyen Duc Kien <kiennd711@gmail.com>
2025-07-29 14:58:08 +0700

    Complete 13_palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let ltr = "";
+    let rtl = "";
+    for (let i = 0; i < str.length; ++i) {
+        if (((str[i] >= "A" && str[i] <= "Z") || (str[i] >= "a" && str[i] <= "z") || (str[i] >= "0" && str[i] <= "9")))
+            ltr = ltr + str[i];
+        }
+    for (let i = str.length; i >= 0; --i) {
+        if ((str[i] >= "A" && str[i] <= "Z") || (str[i] >= "a" && str[i] <= "z") || (str[i] >= "0" && str[i] <= "9"))
+            rtl = rtl + str[i];
+        }
+    if (rtl.toLowerCase() === ltr.toLowerCase()) return true;
+    else return false;
 };
 
 // Do not edit below this line
