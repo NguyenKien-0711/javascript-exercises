19082c1d85fee31e0f6d02f2b793d36c59e7d59e
Nguyen Duc Kien <kiennd711@gmail.com>
2025-07-29 14:32:55 +0700

    Complete 12_calculator

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; ++i) {
+    sum += arr[i];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let mul = 1;
+  for (let i = 0; i < arr.length; ++i) {
+    mul *= arr[i];
+  }
+  return mul;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  let res = 1;
+	for(let i = 0; i < b; ++i) {
+    res = res * a;
+  }
+  return res;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	if (a <= 1) return 1;
+  else return a * factorial (a - 1);
 };
 
 // Do not edit below this line
